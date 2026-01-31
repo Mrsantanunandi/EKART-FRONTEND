6c8a1f5b1b19939412ac60b75d621ef7f0b3d288
SANTANU NANDI <santanu2005nandi@gmail.com>
2026-01-31 18:45:28 +0530

    Fix import path for Button component

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { ArrowLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
