cf2086e9ffa0db63fb5d838e1febcb6896db4a32
SANTANU NANDI <santanu2005nandi@gmail.com>
2026-01-31 19:35:21 +0530

    Update axios base URL to use environment variable

--- src/axiosWithJwt.js
+++ src/axiosWithJwt.js
@@ -7,7 +7,7 @@ import { clearCart } from "@/redux/productSlice";
 import { persistor } from "@/redux/store";
 
 const axiosWithJwt = axios.create({
-  baseURL: "https://frolicking-starburst-33437b.netlify.app/api",
+  baseURL: import.meta.env.VITE_URL,
 });
 
 // ================= REQUEST INTERCEPTOR =================
